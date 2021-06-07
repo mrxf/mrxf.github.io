bdff2805852bb77202731b2d7bc646c9609025e7
mrxf <zhshy123@foxmail.com>
2021-06-07 06:59:50 +0000

    Site updated: 2021-06-07 06:59:50

---
+++ demos/tail-effect/precache-manifest.92f4198d16dbd155cb696017cf2043e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3eba490476a2cfd2c549ed5acd3096",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95f8bb961ec59d32d51d",
+    "url": "./static/css/main.a45fa081.chunk.css"
+  },
+  {
+    "revision": "0245babda58f7bd3296f",
+    "url": "./static/js/2.37a737db.chunk.js"
+  },
+  {
+    "revision": "f987e2ad157747397265aba5686340e4",
+    "url": "./static/js/2.37a737db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f8bb961ec59d32d51d",
+    "url": "./static/js/main.83d78dca.chunk.js"
+  },
+  {
+    "revision": "257069630f4ac60dbfb0",
+    "url": "./static/js/runtime-main.37c10cfa.js"
+  }
+]);
